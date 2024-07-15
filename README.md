750700cbb0088446cf1367d815563d91c16c4201
niahon <niahon3107@gmail.com>
2024-07-15 18:28:39 -0300

    add logos and align them properly in grid

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Landing Page
  Landing page for a generic product
+
+
+# IMAGE CREDITS
+logo1:
+www.insightpest.com/
+
+logo2:
+Sarah Stewart VIDM Logo
+Some rights reserved
+https://flic.kr/p/81nDH2
+https://creativecommons.org/licenses/by/2.0/
+
+logo3:
+rexx gon
+Some rights reserved
+https://flic.kr/p/deSfZZ
+https://creativecommons.org/licenses/by-nc-nd/2.0/
--- script.js
+++ script.js
@@ -1,62 +1,5 @@
 "use strict"
 
-/*
-4 - 
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
-  
-  multiplyNumeric(menu);
-
-function multiplyNumeric(obj) {
-    for (let prop in obj) {
-        if (typeof obj[prop] === 'number') {
-            obj[prop] = (obj[prop] * 2);
-        }   
-    }    
-}
-  
-console.log(menu);
-
-
-3 - 
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
-
-function sumSalary (obj) {
-    let sum = 0;
-    for (let prop in obj) {
-        sum += obj[prop];
-    }
-    return sum;
-}
-
-console.log(sumSalary(salaries));
-
-
-2 - 
-function isEmpty (obj) {
-    let result = true;
-    for (let prop in obj) {
-        if (prop in obj) {
-            result = false;
-        }
-    }
-    return result;
-}
-
-let test = {
-    'pingas': 3
-};
-console.log(isEmpty(test) );
-
-
-
 let elBurg = document.getElementById("burger-button");
 let elMenu = document.getElementById("menu");
 let elCarousel = document.getElementById("carousel");
@@ -70,22 +13,18 @@ elButtons.forEach(element => {
     element.addEventListener("click", moveCarousel);
 });
 
-let arrowArray = 0;
+let arrowCounter = 0;
 
 function moveCarousel(e) {
     if (e.target.classList.contains('left')) {
-        if (arrowArray != 0) {
-            arrowArray -= 1;
+        if (arrowCounter != 0) {
+            arrowCounter -= 1;
         }
-    } else if (arrowArray < (elReview.length - 1)) {
-        arrowArray += 1;
+    } else if (arrowCounter < (elReview.length - 1)) {
+        arrowCounter += 1;
     }
-    console.log(arrowArray);
-    elReview[arrowArray].scrollIntoView({
+    elReview[arrowCounter].scrollIntoView({
         behavior: "smooth",
         block: "nearest"
     });
 }
-console.log(elReview);
-
-*/
